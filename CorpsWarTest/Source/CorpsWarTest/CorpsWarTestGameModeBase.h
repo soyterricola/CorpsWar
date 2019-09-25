5ed76996d516fd8609613d515f67ab2293163de7
terricolas <soyterricola@gmail.com>
2019-09-25 17:32:59 -0500

    Start

---
+++ CorpsWarTest/Source/CorpsWarTest/CorpsWarTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CorpsWarTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CORPSWARTEST_API ACorpsWarTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
